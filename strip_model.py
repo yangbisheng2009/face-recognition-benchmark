ac09e03d39a684d8bd466b98674843bc589d2835
yangbisheng2009 <yangbisheng2009@gmail.com>
2020-12-04 17:28:49 +0800

    strip

--- strip_model.py
+++ strip_model.py
@@ -5,19 +5,24 @@ from torch.utils import data
 from torchvision import transforms as T
 import argparse
 
-from utils.models import *
-from utils.utils import extract_feature, cosin_metric
+from utils.dataset import Dataset
+from utils.utils import AverageMeter, accuracy, calc_num_person
+from models.focal_loss import FocalLoss
+from models.optimizer import InsightFaceOptimizer
+from models.models import resnet18, resnet34, resnet50, resnet101, resnet152, MobileNet, resnet_face18, ArcMarginModel
 
-parser = argparse.ArgumentParser(description='Create FaceDB')
-parser.add_argument('-p', '--project', default='./configs/ccfb.yaml', help='project file')
-parser.add_argument('--checkpoint', default='./checkpoints/92-98.9566438832252.pth', help='checkpoint')
-parser.add_argument('--db', default='./db/train-driver.pth', help='checkpoint')
-parser.add_argument('--input-images', default='./input-images', help='input images')
+parser = argparse.ArgumentParser(description='Strip checkpoint to make it smaller.')
+parser.add_argument('--input-checkpoint', default='', help='input checkpoint')
+parser.add_argument('--output-model', default='', help='output model')
+parser.add_argument('--output-metric_fc', default='', help='output metric_fc')
 args = parser.parse_args()
 
 def main():
-    model = torch.load(args.checkpoint)['model']
-    torch.save(model.module.state_dict(), './fc.pth')
+    model = torch.load(args.input_checkpoint)['model']
+    metric_fc = torch.load(args.input_checkpoint)['metric_fc']
+    torch.save(model.module.state_dict(), args.output_checkpoint)
+    torch.save(metric_fc.module.state_dict(), args.output_metric_fc)
+    
 
 if __name__ == '__main__':
     main()
--- utils/utils.py
+++ utils/utils.py
@@ -11,7 +11,7 @@ from PIL import Image
 
 from align_faces import get_reference_facial_points, warp_and_crop_face
 from config import image_h, image_w
-from mtcnn.detector import detect_faces
+#from mtcnn.detector import detect_faces
 
 
 def _clip_gradient(optimizer, grad_clip):
@@ -100,6 +100,7 @@ def align_face(img_fn, facial5points):
     return dst_img
 
 
+'''
 def get_face_attributes(full_path):
     try:
         img = Image.open(full_path).convert('RGB')
@@ -114,7 +115,7 @@ def get_face_attributes(full_path):
     except:
         pass
     return False, None
-
+'''
 
 def select_central_face(im_size, bounding_boxes):
     width, height = im_size
@@ -133,6 +134,7 @@ def select_central_face(im_size, bounding_boxes):
     return nearest_index
 
 
+'''
 def get_central_face_attributes(full_path):
     try:
         img = Image.open(full_path).convert('RGB')
@@ -147,12 +149,15 @@ def get_central_face_attributes(full_path):
     except:
         pass
     return False, None, None
+'''
 
 
+'''
 def get_all_face_attributes(full_path):
     img = Image.open(full_path).convert('RGB')
     bounding_boxes, landmarks = detect_faces(img)
     return bounding_boxes, landmarks
+'''
 
 
 def draw_bboxes(img, bounding_boxes, facial_landmarks=[]):
